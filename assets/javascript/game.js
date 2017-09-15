bed4a73e2b7db8fc9d0ee979750a0c64bfae3e81
Margaret Rumble <megrumble@gmail.com>
2017-09-15 16:13:26 -0400

    power points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,22 +8,58 @@ var target;
 var white;
 var rainbow;
 var flower;
-var blue; 
+var blue;
 
 var reset = function () {
     number = 0;
     target = Math.floor((Math.random() * 100) + 24);
-    white = Math.floor((Math.random() * 5) + 1);
-    rainbow = Math.floor((Math.random() * 5) + 1);
-    flower = Math.floor((Math.random() * 5) + 1);
-    blue = Math.floor((Math.random() * 5) + 1);
+    white = Math.floor((Math.random() * 9) + 1);
+    rainbow = Math.floor((Math.random() * 9) + 1);
+    flower = Math.floor((Math.random() * 9) + 1);
+    blue = Math.floor((Math.random() * 9) + 1);
+    // checkScore();
 
     $("#target").empty();
     $("#target").text(target);
     console.log(target);
+    console.log(white);
+    console.log(blue);
+    console.log(flower);
+    console.log(rainbow);
+
+
 }
+
+// var checkScore = function() {
+//     if number===target {
+//         $(".number").text("You Win!");
+//         reset();
+//     }else if number > target{
+//         $(".number").text("You Lose");
+//         reset();
+//     }
+//     checkScore();
+
+
 $(".white").on("click", function () {
-  number += white
+    number += white;
+    console.log(number);
+    $(".number").text(number);
+});
+$(".flower").on("click", function () {
+    number += flower;
+    console.log(number);
+    $(".number").text(number);
+});
+$(".rainbow").on("click", function () {
+    number += rainbow;
+    console.log(number);
+    $(".number").text(number);
+});
+$(".blue").on("click", function () {
+    number += blue;
+    console.log(number);
+    $(".number").text(number);
 });
 
 reset();
