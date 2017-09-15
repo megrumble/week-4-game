3f439af0078edfd297bfe2ee7ef82973da64668f
Margaret Rumble <megrumble@gmail.com>
2017-09-15 15:09:25 -0400

    some new work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,29 @@
-var wins= 0;
-var losses =0;
-var math=0;
+var wins = 0;
+var losses = 0;
+var number = 0;
 
-//target number
-var target = Math.floor((Math.random()* 100)+1) ; 
+// //target number
+var target;
 //crystal power number
-var white = Math.floor((Math.random()* 5) + 1);
-var rainbow =Math.floor((Math.random()* 5) + 1);
-var flower = Math.floor((Math.random()* 5) + 1);
-var blue = Math.floor((Math.random()* 5) + 1);
+var white;
+var rainbow;
+var flower;
+var blue; 
 
-var reset = function(){
-	math = 0;
-    target = Math.floor((Math.random()* 100) + 1);
+var reset = function () {
+    number = 0;
+    target = Math.floor((Math.random() * 100) + 24);
+    white = Math.floor((Math.random() * 5) + 1);
+    rainbow = Math.floor((Math.random() * 5) + 1);
+    flower = Math.floor((Math.random() * 5) + 1);
+    blue = Math.floor((Math.random() * 5) + 1);
 
-	$("#target").empty();
-    $("#target").append(target);
+    $("#target").empty();
+    $("#target").text(target);
     console.log(target);
 }
-// reset();
+$(".white").on("click", function () {
+  number += (white)
+});
+
+reset();
